df03a38c0b2ea0a67502d1c59beec9abcec5d212
Ryo Nonogaki <ryononosongs3746@docomo.ne.jp>
2023-08-06 15:55:07 +0900

    added memo

--- src/components/Result.js
+++ src/components/Result.js
@@ -7,7 +7,7 @@ const Result = () => {
   // 下記の記述で、渡ってきたlocation.stateの情報がselectInfoに渡される(const { state} という記述ではエラーが出てしまった)
   const [selectInfo, setSelectInfo] = useState(location.state);
 
-  // setSelectInfo' is assigned a value but never usedというエラーが出続けており心配だったが、値を遷移元から受け取るだけならsetをする必要もないし
+  // setSelectInfo' is assigned a value but never usedというエラーが出続けているが、値を遷移元から受け取るだけならsetをする必要がないし
   // 使用しなくても良さそう
 
   console.log("selected country is : " + selectInfo.country + ", and selected city is " + selectInfo.city);
